d0521e700b0730c1c60e40ee4e367c4a2866c665
Callixte Fusier <fusiercallixte@gmail.com>
2021-11-25 17:06:07 +0100

    page API

---
+++ page/API/API_apod.js
@@ -0,0 +1,25 @@
+// API : https://wheretheiss.at/w/developer
+
+// Clé API
+const API_key2 = 'I9zujY3IIuLDhoyOpvh5K5PDXayBLskqhHS5klFi';
+// Url API 
+const API_URL2 = "https://api.nasa.gov/planetary/apod?api_key=";
+
+var req = new XMLHttpRequest();
+
+
+function showApod(){
+    req.open("GET", API_URL2 + API_key2);
+    req.send();
+
+    req.addEventListener("load", function(){
+        if(req.status == 200 && req.readyState == 4){
+          var response = JSON.parse(req.responseText);
+        document.getElementById("title").textContent = response.title;
+        document.getElementById("date").textContent = response.date;
+        document.getElementById("pic").src = response.hdurl;
+      }
+    })
+}
+
+
