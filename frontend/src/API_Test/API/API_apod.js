413a0a1e0b0e4595ab980cb20400068b33bfadcc
ArthurGIL <49671280+ArthurGIL@users.noreply.github.com>
2021-11-27 20:14:14 +0100

    Test affichage des API

--- frontend/src/API_Test/API/API_apod.js
+++ frontend/src/API_Test/API/API_apod.js
@@ -15,11 +15,10 @@ function showApod(){
     req.addEventListener("load", function(){
         if(req.status == 200 && req.readyState == 4){
           var response = JSON.parse(req.responseText);
-        document.getElementById("title").textContent = response.title;
-        document.getElementById("date").textContent = response.date;
-        document.getElementById("pic").src = response.hdurl;
-      }
+          document.getElementById("title").textContent = response.title;
+          document.getElementById("date").textContent = response.date;
+          document.getElementById("pic").src = response.hdurl;
+        }
     })
 }
-
 
